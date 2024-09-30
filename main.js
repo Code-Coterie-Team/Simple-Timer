9ebf922d2fc5d6734fd12b3d3188ceaf412bb942
Arezoo <arezooayoblo@gmail.com>
2024-09-30 15:39:56 +0330

    js progress

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 let timeleft=60;
 let isPaused=true;
 let timer;
-const timerbtn=document.getElementById('txt');
+const timerbtn=document.getElementById('timer');
 const startpause=document.getElementById('startpause')
 const reset=document.getElementById('reset')
+const root=document.querySelector(':root');
 
-const timerShow=()=>{
+function timerShow(){
     const minute=Math.floor(timeleft/60);
     const second=timeleft%60;
     timerbtn.innerHTML=`${minute}:${second}`;
@@ -17,20 +18,25 @@ startpause.addEventListener('click',()=>{
         isPaused=false;
         startpause.innerHTML='pause';
         timer=setInterval(()=>{
+    
             if(timeleft>0){
                 timeleft--;
                 timerShow()
+                root.style.setProperty('--degrees',circleDegree())
+
             }
             else{
-                clearInterval(timer)
+                
                 startpause.disabled=true;
+                clearInterval(timer);
+                startpause.textContent='start';
+                
             }
         },1000)}
     else {
         isPaused=true;
-        startpause.innerHTML='start';
-        
-
+        startpause.textContent='start';
+        clearInterval(timer);
     }
 
     }
@@ -41,10 +47,16 @@ reset.addEventListener('click',()=>{
     clearInterval(timer);
     timeleft=60;
     isPaused=true;
-    startpause.disabled=false;
     startpause.textContent='start';
+    root.style.setProperty('--degrees','0deg');
     timerShow();
 
+
 })
+
+function circleDegree(){
+    return `${0+((60-timeleft)*6)}deg`;
+}
+timerShow();
 
 
